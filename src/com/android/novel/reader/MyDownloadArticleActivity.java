8cfc4626089561bc439d71713ce0db1fb7028aac
Steven Ko <chunyuko85@gmaill.com>
2013-03-17 18:07:31 +0800

    書櫃刪除小說, finish activity and reload download fragment

--- src/com/android/novel/reader/MyDownloadArticleActivity.java
+++ src/com/android/novel/reader/MyDownloadArticleActivity.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -26,159 +26,153 @@ import com.taiwan.imageload.ImageLoader;
 import com.taiwan.imageload.ListArticleAdapter;
 
 public class MyDownloadArticleActivity extends SherlockFragmentActivity {
-	
-// 	API: 
-//	搜索: searchNovels(String), 
-//	取某篇章節: getArticle(Article), 
-//	取所有章節: getNovelArticles(int novelId, int page, boolean isOrderUp),
-	
-	private static final int ID_SETTING = 0;
-    private static final int ID_RESPONSE = 1;
-    private static final int ID_ABOUT_US = 2;
-    private static final int ID_GRADE = 3;
-    private static final int ID_DELETE_DOWNLOAD = 4;
 
-  	
-	private Bundle mBundle;
-	private String novelName;
-	private int novelId;
-	private String novelAuthor;
-	private String novelPicUrl;
-	private String novelArticleNum;
-	private ImageView novelImageView;
-	private TextView novelTextName;
-	private TextView novelTextAuthor;
-	private TextView downloadedCount;
-	private ImageLoader mImageLoader;
-	private LinearLayout novelLayoutProgress;
-	private ArrayList<Article> articleList = new ArrayList<Article>();
-	private ListView novelListView;
-	
-	private ArrayList<Group> mGroups = new ArrayList<Group>();
-	private AlertDialog.Builder deleteDialog;
-	
-	
+    // API:
+    // 搜索: searchNovels(String),
+    // 取某篇章節: getArticle(Article),
+    // 取所有章節: getNovelArticles(int novelId, int page, boolean isOrderUp),
+
+    private static final int       ID_SETTING         = 0;
+    private static final int       ID_RESPONSE        = 1;
+    private static final int       ID_ABOUT_US        = 2;
+    private static final int       ID_GRADE           = 3;
+    private static final int       ID_DELETE_DOWNLOAD = 4;
+
+    private Bundle                 mBundle;
+    private String                 novelName;
+    private int                    novelId;
+    private String                 novelAuthor;
+    private String                 novelPicUrl;
+    private String                 novelArticleNum;
+    private ImageView              novelImageView;
+    private TextView               novelTextName;
+    private TextView               novelTextAuthor;
+    private TextView               downloadedCount;
+    private ImageLoader            mImageLoader;
+    private LinearLayout           novelLayoutProgress;
+    private ArrayList<Article>     articleList        = new ArrayList<Article>();
+    private ListView               novelListView;
+
+    private final ArrayList<Group> mGroups            = new ArrayList<Group>();
+    private AlertDialog.Builder    deleteDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_novel_downloaded);
-        
-        
-        final ActionBar ab = getSupportActionBar();		     
+
+        final ActionBar ab = getSupportActionBar();
         mBundle = this.getIntent().getExtras();
         novelName = mBundle.getString("NovelName");
         novelId = mBundle.getInt("NovelId");
         novelAuthor = mBundle.getString("NovelAuthor");
         novelPicUrl = mBundle.getString("NovelPicUrl");
         novelArticleNum = mBundle.getString("NovelArticleNum");
-        
+
         ab.setTitle(" 我的下載");
         ab.setDisplayHomeAsUpEnabled(true);
-        
+
         setViews();
-        
+
         new DownloadArticlesTask().execute();
-        
+
     }
 
-	private void setViews() {
-		novelImageView = (ImageView) findViewById (R.id.novel_image);
-		novelTextName = (TextView) findViewById (R.id.novel_name);
-		novelTextAuthor = (TextView) findViewById (R.id.novel_author);
-		novelListView = (ListView) findViewById (R.id.novel_download_artiles_list);
-		downloadedCount =  (TextView) findViewById (R.id.text_downloaded_count);
-		novelLayoutProgress = (LinearLayout) findViewById (R.id.novel_layout_progress);
-		
-		novelTextName.setText(novelName + "(" + novelArticleNum + ")");
-		novelTextAuthor.setText("作者:"+novelAuthor);
-		
-		mImageLoader = new ImageLoader(MyDownloadArticleActivity.this, 70);
-		mImageLoader.DisplayImage(novelPicUrl, novelImageView);
-		
-		deleteDialog = new AlertDialog.Builder(this).setTitle("刪除")
-				.setMessage("是否刪除此小說")
-				.setIcon(R.drawable.app_icon)
-				.setPositiveButton("刪除", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						Novel myNovel = new Novel(novelId, novelName, novelAuthor, "", novelPicUrl, 0, novelArticleNum, "", false, false,true);
-						NovelAPI.removeNovelFromDownload(myNovel, MyDownloadArticleActivity.this);
-				}
-				})
-				.setNegativeButton("不刪除", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-				}
-		});
-		
-	}
-	
+    private void setViews() {
+        novelImageView = (ImageView) findViewById(R.id.novel_image);
+        novelTextName = (TextView) findViewById(R.id.novel_name);
+        novelTextAuthor = (TextView) findViewById(R.id.novel_author);
+        novelListView = (ListView) findViewById(R.id.novel_download_artiles_list);
+        downloadedCount = (TextView) findViewById(R.id.text_downloaded_count);
+        novelLayoutProgress = (LinearLayout) findViewById(R.id.novel_layout_progress);
 
-	@Override
+        novelTextName.setText(novelName + "(" + novelArticleNum + ")");
+        novelTextAuthor.setText("作者:" + novelAuthor);
+
+        mImageLoader = new ImageLoader(MyDownloadArticleActivity.this, 70);
+        mImageLoader.DisplayImage(novelPicUrl, novelImageView);
+
+        deleteDialog = new AlertDialog.Builder(this).setTitle("刪除").setMessage("是否刪除此小說").setIcon(R.drawable.app_icon)
+                .setPositiveButton("刪除", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Novel myNovel = new Novel(novelId, novelName, novelAuthor, "", novelPicUrl, 0, novelArticleNum, "", false, false, true);
+                        NovelAPI.removeNovelFromDownload(myNovel, MyDownloadArticleActivity.this);
+                        MyDownloadArticleActivity.this.finish();
+                    }
+                }).setNegativeButton("不刪除", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-		//getMenuInflater().inflate(R.menu.activity_main, menu);
-	
-		menu.add(0, ID_SETTING, 0, "設定").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_RESPONSE, 1, "意見回餽").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_ABOUT_US, 2, "關於我們").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_GRADE, 3, "為App評分").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_DELETE_DOWNLOAD, 5, "刪除").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        // getMenuInflater().inflate(R.menu.activity_main, menu);
+
+        menu.add(0, ID_SETTING, 0, "設定").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_RESPONSE, 1, "意見回餽").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_ABOUT_US, 2, "關於我們").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_GRADE, 3, "為App評分").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_DELETE_DOWNLOAD, 5, "刪除").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return true;
     }
-	
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 
-	    int itemId = item.getItemId();
-	    switch (itemId) {
-	    case android.R.id.home:
-	        finish();
-	        // Toast.makeText(this, "home pressed", Toast.LENGTH_LONG).show();
-	        break;
-	    case ID_SETTING: // setting
-	    		Intent intent = new Intent(MyDownloadArticleActivity.this, SettingActivity.class);
-	    		startActivity(intent); 
-	        break;
-	    case ID_RESPONSE: // response
-    			Toast.makeText(MyDownloadArticleActivity.this, "RESPONESE", Toast.LENGTH_SHORT).show();
-    		break;
-	    case ID_ABOUT_US: // response
-			Toast.makeText(MyDownloadArticleActivity.this, "ABOUT_US", Toast.LENGTH_SHORT).show();
-			break;
-	    case ID_GRADE: // response
-			Toast.makeText(MyDownloadArticleActivity.this, "GRADE", Toast.LENGTH_SHORT).show();
-			break;
-	    case ID_DELETE_DOWNLOAD: // response
-	    	deleteDialog.show();
-			break;
-	    	
-	    }
-	    return true;
-	}
-	
-	 private class DownloadArticlesTask extends AsyncTask {
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
 
-	        @Override
-	        protected Object doInBackground(Object... params) {	            
-	        	articleList = NovelAPI.getDownloadedNovelArticles(novelId, false, MyDownloadArticleActivity.this);
-	            return null;
-	        }
+        int itemId = item.getItemId();
+        switch (itemId) {
+        case android.R.id.home:
+            finish();
+            // Toast.makeText(this, "home pressed", Toast.LENGTH_LONG).show();
+            break;
+        case ID_SETTING: // setting
+            Intent intent = new Intent(MyDownloadArticleActivity.this, SettingActivity.class);
+            startActivity(intent);
+            break;
+        case ID_RESPONSE: // response
+            Toast.makeText(MyDownloadArticleActivity.this, "RESPONESE", Toast.LENGTH_SHORT).show();
+            break;
+        case ID_ABOUT_US: // response
+            Toast.makeText(MyDownloadArticleActivity.this, "ABOUT_US", Toast.LENGTH_SHORT).show();
+            break;
+        case ID_GRADE: // response
+            Toast.makeText(MyDownloadArticleActivity.this, "GRADE", Toast.LENGTH_SHORT).show();
+            break;
+        case ID_DELETE_DOWNLOAD: // response
+            deleteDialog.show();
+            break;
 
-	        @Override
-	        protected void onPostExecute(Object result) {
-	          
-	            super.onPostExecute(result);
-	            novelLayoutProgress.setVisibility(View.GONE);
-	            if(articleList!= null && articleList.size()!= 0){
-	            	
-	            	ListArticleAdapter mAdapter = new ListArticleAdapter( MyDownloadArticleActivity.this, articleList, novelName);
-	            	novelListView.setAdapter(mAdapter);
-	            
-	            }
-	            String txtCount = Integer.toString(articleList.size());
-	            downloadedCount.setText("共 "+txtCount+" 個下載");
+        }
+        return true;
+    }
 
-	        }
-	 }
-    
+    private class DownloadArticlesTask extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object... params) {
+            articleList = NovelAPI.getDownloadedNovelArticles(novelId, false, MyDownloadArticleActivity.this);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+
+            super.onPostExecute(result);
+            novelLayoutProgress.setVisibility(View.GONE);
+            if (articleList != null && articleList.size() != 0) {
+
+                ListArticleAdapter mAdapter = new ListArticleAdapter(MyDownloadArticleActivity.this, articleList, novelName);
+                novelListView.setAdapter(mAdapter);
+
+            }
+            String txtCount = Integer.toString(articleList.size());
+            downloadedCount.setText("共 " + txtCount + " 個下載");
+
+        }
+    }
 
 }
--- src/com/kosbrother/fragments/MyDownloadFragment.java
+++ src/com/kosbrother/fragments/MyDownloadFragment.java
@@ -11,78 +11,75 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.android.novel.reader.R;
-import com.android.novel.reader.R.id;
-import com.android.novel.reader.R.layout;
 import com.android.novel.reader.api.NovelAPI;
 import com.android.novel.reader.entity.Novel;
-import com.taiwan.imageload.GridViewAdapter;
 import com.taiwan.imageload.GridViewDownloadAdapter;
-import com.taiwan.imageload.ListNothingAdapter;
 import com.taiwan.imageload.LoadMoreGridView;
 
 public class MyDownloadFragment extends Fragment {
-    
-	private ArrayList<Novel> novels = new ArrayList<Novel>();
-	private LoadMoreGridView  myGrid;
-	private GridViewDownloadAdapter myGridViewAdapter;
-	private LinearLayout progressLayout;
-	private LinearLayout loadmoreLayout;
-	private LinearLayout noDataLayout;
-	
-    public static MyDownloadFragment newInstance() {     
- 
-    	MyDownloadFragment fragment = new MyDownloadFragment();
-  	    
-      return fragment;
-        
-    }
 
+    private ArrayList<Novel>        novels = new ArrayList<Novel>();
+    private LoadMoreGridView        myGrid;
+    private GridViewDownloadAdapter myGridViewAdapter;
+    private LinearLayout            progressLayout;
+    private LinearLayout            loadmoreLayout;
+    private LinearLayout            noDataLayout;
+
+    public static MyDownloadFragment newInstance() {
+
+        MyDownloadFragment fragment = new MyDownloadFragment();
+
+        return fragment;
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-              
-        new DownloadChannelsTask().execute();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        
-    	
-    	View myFragmentView = inflater.inflate(R.layout.loadmore_grid, container, false);
-    	progressLayout = (LinearLayout) myFragmentView.findViewById(R.id.layout_progress);
-    	loadmoreLayout = (LinearLayout) myFragmentView.findViewById(R.id.load_more_grid);
-    	noDataLayout = (LinearLayout) myFragmentView.findViewById(R.id.layout_no_data);
-    	myGrid = (LoadMoreGridView) myFragmentView.findViewById(R.id.news_list);
-    	myGrid.setOnLoadMoreListener(new LoadMoreGridView.OnLoadMoreListener() {
-			public void onLoadMore() {
-//				// Do the work to load more items at the end of list
-//				
-//				if(checkLoad){
-//					myPage = myPage +1;
-//					loadmoreLayout.setVisibility(View.VISIBLE);
-//					new LoadMoreTask().execute();
-//				}else{
-//					myGrid.onLoadMoreComplete();
-//				}
-			}
-		});
+
+        View myFragmentView = inflater.inflate(R.layout.loadmore_grid, container, false);
+        progressLayout = (LinearLayout) myFragmentView.findViewById(R.id.layout_progress);
+        loadmoreLayout = (LinearLayout) myFragmentView.findViewById(R.id.load_more_grid);
+        noDataLayout = (LinearLayout) myFragmentView.findViewById(R.id.layout_no_data);
+        myGrid = (LoadMoreGridView) myFragmentView.findViewById(R.id.news_list);
+        myGrid.setOnLoadMoreListener(new LoadMoreGridView.OnLoadMoreListener() {
+            public void onLoadMore() {
+                // // Do the work to load more items at the end of list
+                //
+                // if(checkLoad){
+                // myPage = myPage +1;
+                // loadmoreLayout.setVisibility(View.VISIBLE);
+                // new LoadMoreTask().execute();
+                // }else{
+                // myGrid.onLoadMoreComplete();
+                // }
+            }
+        });
         return myFragmentView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        new DownloadChannelsTask().execute();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-       
+
     }
-    
+
     private class DownloadChannelsTask extends AsyncTask {
 
         @Override
         protected void onPreExecute() {
             // TODO Auto-generated method stub
             super.onPreExecute();
-            
 
         }
 
@@ -90,8 +87,8 @@ public class MyDownloadFragment extends Fragment {
         protected Object doInBackground(Object... params) {
             // TODO Auto-generated method stub
 
-        	novels = NovelAPI.getDownloadedNovels(getActivity());
-//        	moreNovels = NovelAPI.getThisWeekHotNovels(); 
+            novels = NovelAPI.getDownloadedNovels(getActivity());
+            // moreNovels = NovelAPI.getThisWeekHotNovels();
 
             return null;
         }
@@ -101,22 +98,21 @@ public class MyDownloadFragment extends Fragment {
             // TODO Auto-generated method stub
             super.onPostExecute(result);
             progressLayout.setVisibility(View.GONE);
-            loadmoreLayout.setVisibility(View.GONE);          
-            
-            if(novels !=null && novels.size()!=0){
-          	  try{
-          		myGridViewAdapter = new GridViewDownloadAdapter(getActivity(), novels);
-          		myGrid.setAdapter(myGridViewAdapter);
-          	  }catch(Exception e){
-          		 
-          	  }
-            }else{
-            	myGrid.setVisibility(View.GONE);
-            	noDataLayout.setVisibility(View.VISIBLE);
+            loadmoreLayout.setVisibility(View.GONE);
+
+            if (novels != null && novels.size() != 0) {
+                try {
+                    myGridViewAdapter = new GridViewDownloadAdapter(getActivity(), novels);
+                    myGrid.setAdapter(myGridViewAdapter);
+                } catch (Exception e) {
+
+                }
+            } else {
+                myGrid.setVisibility(View.GONE);
+                noDataLayout.setVisibility(View.VISIBLE);
             }
 
         }
     }
-    
-  
+
 }
