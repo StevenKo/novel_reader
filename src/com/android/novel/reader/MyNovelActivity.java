f2ae98927f73ccf2c9d7d306c84d062fd5f00099
Steven Ko <chunyuko85@gmaill.com>
2013-03-18 18:29:15 +0800

    download service

--- src/com/android/novel/reader/MyNovelActivity.java
+++ src/com/android/novel/reader/MyNovelActivity.java
@@ -1,7 +1,5 @@
 package com.android.novel.reader;
 
-import java.io.IOException;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -22,36 +20,35 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.adwhirl.AdWhirlLayout;
+import com.adwhirl.AdWhirlLayout.AdWhirlInterface;
 import com.adwhirl.AdWhirlManager;
 import com.adwhirl.AdWhirlTargeting;
-import com.adwhirl.AdWhirlLayout.AdWhirlInterface;
-import com.android.novel.reader.api.Setting;
 import com.google.ads.AdView;
 import com.kosbrother.fragments.MyBookcaseFragment;
 import com.kosbrother.fragments.MyDownloadFragment;
 import com.viewpagerindicator.TitlePageIndicator;
 
-public class MyNovelActivity extends SherlockFragmentActivity implements AdWhirlInterface{
-	
-	private static final int ID_SETTING = 0;
-    private static final int ID_RESPONSE = 1;
-    private static final int ID_ABOUT_US = 2;
-    private static final int ID_GRADE = 3;
-    private String[] CONTENT;
-    private AlertDialog.Builder aboutUsDialog;	
-    private String adWhirlKey = "215f895eb71748e7ba4cb3a5f20b061e";
-    private ViewPager pager;
+public class MyNovelActivity extends SherlockFragmentActivity implements AdWhirlInterface {
+
+    private static final int          ID_SETTING  = 0;
+    private static final int          ID_RESPONSE = 1;
+    private static final int          ID_ABOUT_US = 2;
+    private static final int          ID_GRADE    = 3;
+    private String[]                  CONTENT;
+    private AlertDialog.Builder       aboutUsDialog;
+    private final String              adWhirlKey  = "215f895eb71748e7ba4cb3a5f20b061e";
+    private ViewPager                 pager;
     private FragmentStatePagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_titles);
-        
+
         final ActionBar ab = getSupportActionBar();
         ab.setTitle(getResources().getString(R.string.title_my_bookcase));
         ab.setDisplayHomeAsUpEnabled(true);
-        
+
         Resources res = getResources();
         CONTENT = res.getStringArray(R.array.collections);
 
@@ -60,23 +57,29 @@ public class MyNovelActivity extends SherlockFragmentActivity implements AdWhirl
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(adapter);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            if (extras.getBoolean("noti")) {
+                pager.setCurrentItem(1);
+            }
+        }
+
         TitlePageIndicator indicator = (TitlePageIndicator) findViewById(R.id.indicator);
         indicator.setViewPager(pager);
-        
+
         setAboutUsDialog();
-        try{
-			Display display = getWindowManager().getDefaultDisplay(); 
-			int width = display.getWidth();  // deprecated
-			int height = display.getHeight();  // deprecated
-		
-			if (width > 320){
-				setAdAdwhirl();
-			}
-		}catch(Exception e){
-			
-		}
-        
-        
+        try {
+            Display display = getWindowManager().getDefaultDisplay();
+            int width = display.getWidth(); // deprecated
+            int height = display.getHeight(); // deprecated
+
+            if (width > 320) {
+                setAdAdwhirl();
+            }
+        } catch (Exception e) {
+
+        }
+
     }
 
     @Override
@@ -84,56 +87,57 @@ public class MyNovelActivity extends SherlockFragmentActivity implements AdWhirl
         // Inflate the menu; this adds items to the action bar if it is present.
         // getMenuInflater().inflate(R.menu.activity_main, menu);
 
-    	menu.add(0, ID_SETTING, 0, getResources().getString(R.string.menu_settings)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_RESPONSE, 1, getResources().getString(R.string.menu_respond)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_ABOUT_US, 2, getResources().getString(R.string.menu_aboutus)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-		menu.add(0, ID_GRADE, 3, getResources().getString(R.string.menu_recommend)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_SETTING, 0, getResources().getString(R.string.menu_settings)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_RESPONSE, 1, getResources().getString(R.string.menu_respond)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_ABOUT_US, 2, getResources().getString(R.string.menu_aboutus)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(0, ID_GRADE, 3, getResources().getString(R.string.menu_recommend)).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
 
         return true;
     }
-    
+
+    @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
 
-	    int itemId = item.getItemId();
-	    switch (itemId) {
-	    case android.R.id.home:
-	        finish();
-	        // Toast.makeText(this, "home pressed", Toast.LENGTH_LONG).show();
-	        break;
-	    case ID_SETTING: // setting
-    		Intent intent = new Intent(MyNovelActivity.this, SettingActivity.class);
-    		startActivity(intent); 
-    		break;
-	    case ID_RESPONSE: // response
-	    	final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-	    	emailIntent.setType("plain/text");
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{getResources().getString(R.string.respond_mail_address)});
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getResources().getString(R.string.respond_mail_title));
-	    	emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
-	    	startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-			break;
-	    case ID_ABOUT_US: 
-	    	aboutUsDialog.show();
-			break;
-	    case ID_GRADE: 
-	    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.recommend_url)));
-			startActivity(browserIntent);
-			break;
-	    }
-	    return true;
-	}
+        int itemId = item.getItemId();
+        switch (itemId) {
+        case android.R.id.home:
+            finish();
+            // Toast.makeText(this, "home pressed", Toast.LENGTH_LONG).show();
+            break;
+        case ID_SETTING: // setting
+            Intent intent = new Intent(MyNovelActivity.this, SettingActivity.class);
+            startActivity(intent);
+            break;
+        case ID_RESPONSE: // response
+            final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+            emailIntent.setType("plain/text");
+            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { getResources().getString(R.string.respond_mail_address) });
+            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getResources().getString(R.string.respond_mail_title));
+            emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            break;
+        case ID_ABOUT_US:
+            aboutUsDialog.show();
+            break;
+        case ID_GRADE:
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.recommend_url)));
+            startActivity(browserIntent);
+            break;
+        }
+        return true;
+    }
 
     class NovelPagerAdapter extends FragmentStatePagerAdapter {
         public NovelPagerAdapter(FragmentManager fm) {
             super(fm);
         }
-        
+
         // 這邊要寫兩個不同的Fragment, 分別取書櫃資 & 下載資料
         @Override
         public Fragment getItem(int position) {
             Fragment kk = new Fragment();
             if (position == 0) {
-            	kk = MyBookcaseFragment.newInstance();
+                kk = MyBookcaseFragment.newInstance();
             } else if (position == 1) {
                 kk = MyDownloadFragment.newInstance();
             }
@@ -150,74 +154,72 @@ public class MyNovelActivity extends SherlockFragmentActivity implements AdWhirl
             return CONTENT.length;
         }
     }
-    
+
     private void setAboutUsDialog() {
-		// TODO Auto-generated method stub
-    	aboutUsDialog = new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.about_us_string))
-				.setIcon(R.drawable.play_store_icon)
-				.setMessage(getResources().getString(R.string.about_us))
-				.setPositiveButton(getResources().getString(R.string.yes_string), new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						
-					}
-				});
-	}
-    
+        // TODO Auto-generated method stub
+        aboutUsDialog = new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.about_us_string)).setIcon(R.drawable.play_store_icon)
+                .setMessage(getResources().getString(R.string.about_us))
+                .setPositiveButton(getResources().getString(R.string.yes_string), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+    }
+
     @Override
-	 protected void onResume() {
-		super.onResume();
-		int position = pager.getCurrentItem();
-		pager.setAdapter(adapter);
-		pager.setCurrentItem(position);
-     }		
-	 
-    
+    protected void onResume() {
+        super.onResume();
+        int position = pager.getCurrentItem();
+        pager.setAdapter(adapter);
+        pager.setCurrentItem(position);
+    }
+
     private void setAdAdwhirl() {
-		// TODO Auto-generated method stub
-		AdWhirlManager.setConfigExpireTimeout(1000 * 60); 
+        // TODO Auto-generated method stub
+        AdWhirlManager.setConfigExpireTimeout(1000 * 60);
         AdWhirlTargeting.setAge(23);
         AdWhirlTargeting.setGender(AdWhirlTargeting.Gender.MALE);
         AdWhirlTargeting.setKeywords("online games gaming");
         AdWhirlTargeting.setPostalCode("94123");
         AdWhirlTargeting.setTestMode(false);
-   		
-        AdWhirlLayout adwhirlLayout = new AdWhirlLayout(this, adWhirlKey);	
 
-        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.adonView);
-        
-    	adwhirlLayout.setAdWhirlInterface(this);
-	 	 	
-	 	mainLayout.addView(adwhirlLayout);
-		
-		mainLayout.invalidate();
+        AdWhirlLayout adwhirlLayout = new AdWhirlLayout(this, adWhirlKey);
+
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.adonView);
+
+        adwhirlLayout.setAdWhirlInterface(this);
+
+        mainLayout.addView(adwhirlLayout);
+
+        mainLayout.invalidate();
     }
 
-	@Override
-	public void adWhirlGeneric() {
-			// TODO Auto-generated method stub
-			
-	}
-		
-	public void rotationHoriztion(int beganDegree, int endDegree, AdView view) {
-			final float centerX = 320 / 2.0f;
-			final float centerY = 48 / 2.0f;
-			final float zDepth = -0.50f * view.getHeight();
-		
-			Rotate3dAnimation rotation = new Rotate3dAnimation(beganDegree, endDegree, centerX, centerY, zDepth, true);
-			rotation.setDuration(1000);
-			rotation.setInterpolator(new AccelerateInterpolator());
-			rotation.setAnimationListener(new Animation.AnimationListener() {
-				public void onAnimationStart(Animation animation) {
-				}
-		
-				public void onAnimationEnd(Animation animation) {
-				}
-		
-				public void onAnimationRepeat(Animation animation) {
-				}
-			});
-			view.startAnimation(rotation);
-		}
+    @Override
+    public void adWhirlGeneric() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void rotationHoriztion(int beganDegree, int endDegree, AdView view) {
+        final float centerX = 320 / 2.0f;
+        final float centerY = 48 / 2.0f;
+        final float zDepth = -0.50f * view.getHeight();
+
+        Rotate3dAnimation rotation = new Rotate3dAnimation(beganDegree, endDegree, centerX, centerY, zDepth, true);
+        rotation.setDuration(1000);
+        rotation.setInterpolator(new AccelerateInterpolator());
+        rotation.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation) {
+            }
+
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        view.startAnimation(rotation);
+    }
 
 }
