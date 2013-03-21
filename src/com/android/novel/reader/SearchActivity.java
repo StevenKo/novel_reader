22a73b46b1736013c40928a7545c885af9f47606
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2013-03-21 17:06:56 +0800

    add ad to searchActivity

--- src/com/android/novel/reader/SearchActivity.java
+++ src/com/android/novel/reader/SearchActivity.java
@@ -12,10 +12,13 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Display;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -32,11 +35,17 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.adwhirl.AdWhirlLayout;
+import com.adwhirl.AdWhirlManager;
+import com.adwhirl.AdWhirlTargeting;
+import com.adwhirl.AdWhirlLayout.AdWhirlInterface;
 import com.android.novel.reader.api.NovelAPI;
 import com.android.novel.reader.entity.Novel;
+import com.google.ads.AdView;
+import com.kosbrother.tool.DetectScrollView.DetectScrollViewListener;
 import com.taiwan.imageload.ImageLoader;
 
-public class SearchActivity extends SherlockListActivity {
+public class SearchActivity extends SherlockListActivity implements AdWhirlInterface{
 	
 	private static final int ID_SETTING = 0;
     private static final int ID_RESPONSE = 1;
@@ -52,6 +61,7 @@ public class SearchActivity extends SherlockListActivity {
     
     private LinearLayout layoutNoSearch;
     private AlertDialog.Builder aboutUsDialog;
+    private String adWhirlKey = "215f895eb71748e7ba4cb3a5f20b061e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +98,18 @@ public class SearchActivity extends SherlockListActivity {
         
         setAboutUsDialog();
         new LoadDataTask().execute();
+        
+        try{
+			Display display = getWindowManager().getDefaultDisplay(); 
+			int width = display.getWidth();  // deprecated
+			int height = display.getHeight();  // deprecated
+		
+			if (width > 320){
+				setAdAdwhirl();
+			}
+		}catch(Exception e){
+			
+		}
     }
 
     private void fetchData() {
@@ -295,6 +317,52 @@ public class SearchActivity extends SherlockListActivity {
 						
 					}
 				});
+	}
+    
+private void setAdAdwhirl() {
+		
+		AdWhirlManager.setConfigExpireTimeout(1000 * 60); 
+        AdWhirlTargeting.setAge(23);
+        AdWhirlTargeting.setGender(AdWhirlTargeting.Gender.MALE);
+        AdWhirlTargeting.setKeywords("online games gaming");
+        AdWhirlTargeting.setPostalCode("94123");
+        AdWhirlTargeting.setTestMode(false);
+   		
+        AdWhirlLayout adwhirlLayout = new AdWhirlLayout(this, adWhirlKey);	
+
+        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.adonView);
+        
+    	adwhirlLayout.setAdWhirlInterface(this);
+	 	 	
+	 	mainLayout.addView(adwhirlLayout);
+		
+		mainLayout.invalidate();
+    }
+
+	@Override
+	public void adWhirlGeneric() {
+			
+	}
+		
+	public void rotationHoriztion(int beganDegree, int endDegree, AdView view) {
+			final float centerX = 320 / 2.0f;
+			final float centerY = 48 / 2.0f;
+			final float zDepth = -0.50f * view.getHeight();
+		
+			Rotate3dAnimation rotation = new Rotate3dAnimation(beganDegree, endDegree, centerX, centerY, zDepth, true);
+			rotation.setDuration(1000);
+			rotation.setInterpolator(new AccelerateInterpolator());
+			rotation.setAnimationListener(new Animation.AnimationListener() {
+				public void onAnimationStart(Animation animation) {
+				}
+		
+				public void onAnimationEnd(Animation animation) {
+				}
+		
+				public void onAnimationRepeat(Animation animation) {
+				}
+			});
+			view.startAnimation(rotation);
 	}
    
 
