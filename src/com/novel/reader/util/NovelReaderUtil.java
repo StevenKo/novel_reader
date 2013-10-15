d4e40a6258487cc0039c776e94563a9a2ee10bee
Steven Ko <chunyuko85@gmail.com>
2013-10-15 11:46:57 +0800

    novel image util

--- src/com/novel/reader/util/NovelReaderUtil.java
+++ src/com/novel/reader/util/NovelReaderUtil.java
@@ -7,7 +7,7 @@ import android.content.Context;
 public class NovelReaderUtil {
 
 	public static boolean isDisplayDefaultBookCover(String url){
-		if(url.equals("") || url == null || url.equals("http://www.bestory.com/pics/0.jpg"))
+		if(url.equals("") || url == null || url.equals("null") || url.equals("http://www.bestory.com/pics/0.jpg"))
 			return true;
 		
 		return false;
